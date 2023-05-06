ad5f229a6eef7fc3a76fffcded8aad379bc079cc
Max <likha.max@gmail.com>
2023-05-06 21:51:25 +0300

    feat: add solution for 04-copy-directoy task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,9 +2,20 @@ const path = require('path');
 const fs = require('fs').promises;
 const pathToFolder = path.join(__dirname, 'files');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-  if (err) throw err;
-});
-fs.readdir(pathToFolder).then((files) => {
-  console.log(files);
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+fs.readdir(path.join(__dirname, 'files-copy')).then((files) => {
+  for (i = 0; i < files.length; i += 1) {
+    const dist = path.resolve(__dirname, 'files-copy', files[i]);
+    fs.unlink(dist);
+  }
+  fs.readdir(pathToFolder).then((files) => {
+    for (i = 0; i < files.length; i += 1) {
+      const src = path.join(pathToFolder, files[i]);
+      const dist = path.resolve(__dirname, 'files-copy', files[i]);
+      fs.writeFile(dist, '', (err) => {
+        if (err) throw err;
+      });
+      fs.copyFile(src, dist);
+    }
+  });
 });
